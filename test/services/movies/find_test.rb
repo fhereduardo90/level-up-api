30897a5c0c1774cfa402777db427290e94ca1df6
Fernando Juarez <fhereduardo90@gmail.com>
2020-02-19 10:15:31 -0600

    :white_check_mark: Tests
    Update tests

--- test/services/movies/find_test.rb
+++ test/services/movies/find_test.rb
@@ -3,13 +3,12 @@ require 'test_helper'
 module Movies
   class FindTest < ActiveSupport::TestCase
     test 'should return a list of movies' do
-      star_wars = movies(:star_wars)
-      lion_king = movies(:lion_king)
+      movies = movies()
 
       result = ::MovieServices::Find.call
 
       assert_not_empty(result)
-      assert_equal(result.map(&:id), [star_wars.id, lion_king.id])
+      assert_equal(result.map(&:id), movies.map(&:id))
     end
   end
 end
--- test/services/users/find_test.rb
+++ test/services/users/find_test.rb
@@ -3,13 +3,12 @@ require 'test_helper'
 module Users
   class FindTest < ActiveSupport::TestCase
     test 'should return a list of users' do
-      john = users(:john)
-      dan = users(:dan)
+      users = users()
 
       result = ::UserServices::Find.call
 
       assert_not_empty(result)
-      assert_equal(result.map(&:id), [john.id, dan.id])
+      assert_equal(result.map(&:id), users.map(&:id))
     end
   end
 end
