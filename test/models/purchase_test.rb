5a8c4be9e4e9a8f50f620179a69b7291f1284b20
Fernando Juarez <fhereduardo90@gmail.com>
2020-02-19 12:10:48 -0600

    :white_check_mark: Purchases Tests
    add tests for purchase model validations

---
+++ test/models/purchase_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class PurchaseTest < ActiveSupport::TestCase
+  test 'should not be valid with a quantity less than 1' do
+    purchase = Purchase.new(quantity: 0)
+    purchase.valid?
+
+    assert_includes purchase.errors.messages[:quantity], I18n.t('errors.messages.greater_than', count: 0)
+  end
+
+  test 'should be valid with a quantity greater than 0' do
+    purchase = Purchase.new(quantity: 1)
+    purchase.valid?
+
+    assert_not_includes purchase.errors.messages[:quantity], I18n.t('errors.messages.greater_than', count: 0)
+  end
+
+  test 'should not be valid without an associated user' do
+    purchase = Purchase.new
+    purchase.valid?
+
+    assert_includes purchase.errors.messages[:user], I18n.t('errors.messages.required')
+  end
+
+  test 'should be valid if an user is provided' do
+    john = users(:john)
+    purchase = Purchase.new(user: john)
+    purchase.valid?
+
+    assert_not_includes purchase.errors.messages[:user], I18n.t('errors.messages.required')
+  end
+
+  test 'should not be valid without an associated movie' do
+    purchase = Purchase.new
+    purchase.valid?
+
+    assert_includes purchase.errors.messages[:movie], I18n.t('errors.messages.required')
+  end
+
+  test 'should be valid if a movie is provided' do
+    star_wars = movies(:star_wars)
+    purchase = Purchase.new(movie: star_wars)
+    purchase.valid?
+
+    assert_not_includes purchase.errors.messages[:movie], I18n.t('errors.messages.required')
+  end
+end
