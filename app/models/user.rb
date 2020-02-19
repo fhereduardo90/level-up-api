84bb6cbc92a248f800f075646059d077f4277887
Fernando Juarez <fhereduardo90@gmail.com>
2020-02-19 09:57:26 -0600

    :sparkles: Devise
    * Enable confirmable module.
    * Run 'generate_confirmation_token' function before the record is save.
    * Do not require confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable #, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validates :first_name, :last_name, presence: true
   validates :password, confirmation: true
   validate :valid_age
+
+  before_save :generate_confirmation_token
 
   private
 
@@ -16,5 +18,9 @@ class User < ApplicationRecord
     if birthdate > 18.years.ago.to_date
       errors.add(:birthdate, I18n.t('errors.messages.age', age: 18))
     end
+  end
+
+  def confirmation_required?
+    false
   end
 end
